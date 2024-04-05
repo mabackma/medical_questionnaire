d364ecf1fef140ae34033ace96b896c0b10e2cbb
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-04-06 02:37:52 +0300

    added ui for user information inputs

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -27,16 +27,71 @@ class InputPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _textEditingController,
-              textAlign: TextAlign.center,
-              decoration: const InputDecoration(
-                hintText: 'Anna käyttäjänimesi',
-                hintStyle: TextStyle(color: Colors.white),
+            SizedBox(
+              width: 300.0,
+              child: TextField(
+                controller: _textEditingController,
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: 'Anna käyttäjänimesi',
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                style: const TextStyle(color: Colors.white),
               ),
-              style: const TextStyle(color: Colors.white),
             ),
-            const SizedBox(height: 20.0),
+            const SizedBox(height: 40.0),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Ikä: ',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                SizedBox(width: 10.0),
+                SizedBox(
+                  width: 100.0,
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Anna ikäsi',
+                      hintStyle: TextStyle(color: Colors.white),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40.0),
+            const Text(
+              'Valitse sukupuolesi: ',
+              style: TextStyle(color: Colors.white, fontSize: 20.0),
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    print('Male icon pressed');
+                  },
+                  icon: const Icon(
+                    Icons.male,
+                    color: Colors.lightBlueAccent,
+                    size: 40,
+                  ),
+                ),
+                SizedBox(width: 40.0),
+                IconButton(
+                  onPressed: () {
+                    print('Female icon pressed');
+                  },
+                  icon: const Icon(
+                    Icons.female,
+                    color: Colors.pinkAccent,
+                    size: 40,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40.0),
             ElevatedButton(
               onPressed: () => handleButtonPress(context),
               child: const Text('Lähetä'),
